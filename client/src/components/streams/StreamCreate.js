791bc07da928ff2dfd9d8221f610ffbdb4c86faf
galedesma <gonza.a.ledesma@gmail.com>
2021-03-01 21:15:30 -0300

    Añado API server
    Creo Carpeta api
    Instalo json server
    Edito componente StreamCreate.
    Creo nuevos action creators.
    Instalo dependencias.

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends Component {
   renderError({ error, touched }) {
@@ -24,7 +26,9 @@ class StreamCreate extends Component {
     );
   };
 
-  onSubmit(formValues) {}
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
+  };
 
   render() {
     return (
@@ -58,7 +62,9 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate: validate,
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
